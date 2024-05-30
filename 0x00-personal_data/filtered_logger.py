802412273d1b18f673d9fe7a8ac422f8a1320dd0
wahujos <jwahu30@gmail.com>
2024-05-30 10:57:20 +0300

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,9 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
     """function that returns the log message obfuscated:"""
     pattern = '|'.join([f'{field}=.*?(?={separator}|$)' for field in fields])
     return re.sub(pattern, lambda m:
