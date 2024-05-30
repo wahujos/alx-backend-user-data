aeccdd44df342c96fda4e049108a9a0e3b07d913
wahujos <jwahu30@gmail.com>
2024-05-31 01:13:23 +0300

    question 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,11 @@ def hash_password(password: str) -> bytes:
     """
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(14))
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Use bcrypt to validate that the provided
+    password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
