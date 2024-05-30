469cf5118dd25ffef3c4b483d835b9cd8f54c115
wahujos <jwahu30@gmail.com>
2024-05-30 12:11:45 +0300

    1-question

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,31 @@ import re
 from typing import List
 
 
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        # NotImplementedError
+        message = super().format(record)
+        redacted_message = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        return redacted_message
+
+
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str
+    fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
     """function that returns the log message obfuscated:"""
     pattern = '|'.join([f'{field}=.*?(?={separator}|$)' for field in fields])
