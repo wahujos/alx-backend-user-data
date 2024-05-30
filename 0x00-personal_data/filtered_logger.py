ac3ea76470b8c7d10b0566a28bb49bd42ee0ef5f
wahujos <jwahu30@gmail.com>
2024-05-30 12:13:43 +0300

    1-question

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,11 +16,13 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Doc string for the initialization"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         # NotImplementedError
+        """This function overides the default format method"""
         message = super().format(record)
         redacted_message = filter_datum(
             self.fields, self.REDACTION, message, self.SEPARATOR)
