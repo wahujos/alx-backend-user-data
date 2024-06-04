3be523c67dd4a96c4bca22d4609bc0fe7e1718f7
wahujos <jwahu30@gmail.com>
2024-06-04 23:46:26 +0300

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,18 @@ class Auth():
         """
         another function
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path.endswith('/'):
+            path = path[:-1]
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                excluded_path = excluded_path[:-1]
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
