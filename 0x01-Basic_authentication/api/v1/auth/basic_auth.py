1baaeb5287922784191f130737299ee9a5dca842
wahujos <jwahu30@gmail.com>
2024-06-06 22:21:14 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-"""
-necessary imports
-"""
-from auth import Auth
+"""necessary imports required for the code"""
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """
-    class inherits from auth
-    """
+    """class inherits from auth implements the basic authentication"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """
+        extract_base64_authorization_header
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[len('Basic '):]
