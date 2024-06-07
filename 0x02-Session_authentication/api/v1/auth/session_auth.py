156466fe81c48e11dc4f98c795050c25ac15425e
wahujos <jwahu30@gmail.com>
2024-06-08 02:47:08 +0300

    qn 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Iporting the necessary modules
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 from uuid import uuid4
 
 
@@ -20,6 +21,16 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        session_Id = uuid4()
-        self.user_id_by_session_id[session_Id] = user_id
-        return session_Id
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        instance method that returns a User ID based on a Session ID:
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
