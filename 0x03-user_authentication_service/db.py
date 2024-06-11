2b42689e28640800d64237fc9b4a6e5bd17c18fa
wahujos <jwahu30@gmail.com>
2024-06-12 02:50:44 +0300

    qn 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,7 +32,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         method should save the user to the database.
         """
