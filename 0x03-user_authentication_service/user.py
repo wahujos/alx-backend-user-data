39767df536b1b6611c1c22de1dd573e142a9fea0
wahujos <jwahu30@gmail.com>
2024-06-12 01:36:38 +0300

    qn 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 Base = declarative_base()
 
+
 class User(Base):
     """
     user class maps to the table in the db
