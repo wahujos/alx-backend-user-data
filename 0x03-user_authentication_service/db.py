b687a6bc9392eba88aa131af5b720705974ed95f
wahujos <jwahu30@gmail.com>
2024-06-12 04:00:10 +0300

    qn 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 # Added some imports
@@ -40,3 +42,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwags) -> User:
+        """
+         returns the first row found in the users
+         table as filtered by the method’s input arguments.
+        """
+        try:
+            return self._session.query(User).filter_by(**kwags).one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
