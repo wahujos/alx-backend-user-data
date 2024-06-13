a83ee7fc66b72748f909ead7b5a041c2596d2133
wahujos <jwahu30@gmail.com>
2024-06-13 18:45:55 +0300

    qn 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,9 @@
 """
 documenting the necessary imports
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
 app = Flask(__name__)
 
 AUTH = Auth()
@@ -30,6 +31,23 @@ def post_users():
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    The request is expected to contain form data with
+    "email" and a "password" fields.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if Auth.valid_login(email, password):
+        new_session = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", new_session)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
