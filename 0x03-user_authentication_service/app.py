965932b5295f696c529f66372a604c752b26ce4e
wahujos <jwahu30@gmail.com>
2024-06-13 13:47:35 +0300

    qn 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,11 @@
 """
 documenting the necessary imports
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
 
+AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
@@ -12,6 +14,21 @@ def hello_world():
     function to invoke
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def post_users():
+    """
+    implement the end-point to register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
