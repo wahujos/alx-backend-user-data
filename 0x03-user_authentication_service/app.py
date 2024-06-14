882f93bd02583df320e48cfdeb51bee1bbce1305
wahujos <jwahu30@gmail.com>
2024-06-15 00:18:22 +0300

    qn 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,18 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """
+    implement a get_reset_password_token function
+    to respond to the POST /reset_password route.
+    """
+    email = request.form.get('email')
+    if not email:
+        abort(403)
+    token = AUTH.get_reset_password_token(email)
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
