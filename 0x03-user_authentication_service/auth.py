5f23940d7bb6798ea849954f03d02d93c9496e12
wahujos <jwahu30@gmail.com>
2024-06-14 23:49:54 +0300

    qn 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,3 +101,15 @@ class Auth:
             return None
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        implement the Auth.get_reset_password_token method.
+        It take an email string argument and returns a string.
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            raise ValueError()
+        uuid = _generate_uuid()
+        self._db.update_user(user.id, reset_token=uuid)
+        return uuid
