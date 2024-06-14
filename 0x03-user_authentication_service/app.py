a2cbe0780178f39c73ee4130055b2d639fbb8644
wahujos <jwahu30@gmail.com>
2024-06-14 22:56:46 +0300

    qn 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -56,6 +56,8 @@ def logout():
     as a cookie with key "session_id".
     """
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
