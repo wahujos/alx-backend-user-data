c5fb7364cfeef2ccf17571574d110626e45c60f7
wahujos <jwahu30@gmail.com>
2024-06-15 01:28:12 +0300

    qn 18 update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -122,7 +122,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password)
+            hashed_password = _hash_password(password).decode('utf-8')
             self._db.update_user(
                 user.id, hashed_password=hashed_password, reset_token=None
                 )
